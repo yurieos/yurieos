3e89260f2d41f26304a3bb2208f41838416d6a61
yurieos <os@yurie.ai>
2026-01-16 20:23:56 -0600

    style: widen chat input padding

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -200,7 +200,7 @@ export function ChatPanel({
     <div
       className={cn(
         'w-full bg-background group/form-container shrink-0',
-        messages.length > 0 ? 'sticky bottom-0 px-6 pb-4' : 'px-6'
+        messages.length > 0 ? 'sticky bottom-0 px-4 pb-4' : 'px-4'
       )}
     >
       {messages.length === 0 && (
